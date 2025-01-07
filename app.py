e2ff1cbbf716fcbc4ff268b81c51cb7faaaf50d2
ashutosh <ashutoshjatav19@gmail.com>
2025-01-07 15:09:27 +0530

    add jobitem file for easier data access and do not mess in home.html file also added jsonify to show the data in the json object

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {'id':1,
+   'Role':'Data Analyst',
+   'location': 'Bangluru, India',
+   'salary': 'Rs 12,00,000'
+  },
+  {'id':2,
+   'Role':'Data Scientist',
+    'location': 'Chennai, India',
+    'salary': 'Rs 15,00,000'
+  },
+  {'id':3,
+   'Role':'Front-End',
+    'location': 'Delhi, India',
+    'salary': 'Rs 10,00,000'
+  },
+  {'id':4,
+   'Role':'Back-End',
+    'location': 'Remote',
+    'salary': 'Rs 15,00,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS,static_folder='static',
+                         company_name='Learners')
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
