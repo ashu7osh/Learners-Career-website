eeb31a44a8b70a4714f5727bc70f36ea9c9d8f70
ashutosh <ashutoshjatav19@gmail.com>
2025-02-10 21:54:52 +0530

    completed basic front end work of the website and added some features also

--- app.py
+++ app.py
@@ -4,31 +4,30 @@ app = Flask(__name__)
 
 JOBS = [
   {'id':1,
-   'Role':'Data Analyst',
+   'title':'Data Analyst',
    'location': 'Bangluru, India',
    'salary': 'Rs 12,00,000'
   },
   {'id':2,
-   'Role':'Data Scientist',
-    'location': 'Chennai, India',
-    'salary': 'Rs 15,00,000'
+   'title':'Data Scientist',
+   'location': 'Chennai, India',
+   'salary': 'Rs 15,00,000'
   },
   {'id':3,
-   'Role':'Front-End',
-    'location': 'Delhi, India',
-    'salary': 'Rs 10,00,000'
+   'title':'Front-End',
+   'location': 'Delhi, India',
+   'salary': 'Rs 10,00,000'
   },
   {'id':4,
-   'Role':'Back-End',
-    'location': 'Remote',
-    'salary': 'Rs 15,00,000'
+   'title':'Back-End',
+   'location': 'Remote',
+   'salary': 'Rs 15,00,000'
   }
 ]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html',jobs=JOBS,static_folder='static',
-                         company_name='Learners')
+  return render_template('home.html',jobs=JOBS,static_folder='static', company_name='Learners')
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
